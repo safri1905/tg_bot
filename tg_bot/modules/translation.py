0cde72813247c3b79c2bc2e72c43cd65f8b47311
safri1905 <47934961+safri1905@users.noreply.github.com>
2020-09-11 17:17:42 +0700

    Update translation.py

--- tg_bot/modules/translation.py
+++ tg_bot/modules/translation.py
@@ -55,6 +55,6 @@ __help__ = """
 __mod_name__ = "Translator"
 
 
-TRANSLATE_HANDLER = DisableAbleCommandHandler('t', translate)
+TRANSLATE_HANDLER = CommandHandler('t', translate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
